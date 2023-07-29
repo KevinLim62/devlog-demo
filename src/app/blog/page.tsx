5626b917b651a451b580aff6f683824c882b8292
KevinLim62 <cheryiong62@gmail.com>
2023-07-29 21:45:23 +0800

    devlog-demo rev03

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,18 +8,13 @@ import { getAllTaxonomy, getTaxonomy } from "@/lib/utils/taxonomParser";
 
 const blog_folder = "blog";
 
-const page = ({ searchParams } : { searchParams: {[key: string]:string | string[] | undefined}}) => {
+const page = () => {
 
-       const page = searchParams['page'] ?? '1'
-       const per_page = searchParams['per_page'] ?? '4'
-       const start = (Number(page) - 1) * Number(per_page) // 0, 5, 10 ...
-       const end = start + Number(per_page) // 5, 10, 15 ...
        const allCategories = getAllTaxonomy(blog_folder, "categories");
        const categories = getTaxonomy(blog_folder, "categories");
        const tags = getTaxonomy(blog_folder, "tags");
        const postIndex = getSinglePost("_index.md");
        const posts =  getAllPosts("blog");
-       const totalPages = Math.ceil(posts.length / Number(per_page));
        
       return (
            <>
@@ -27,12 +22,7 @@ const page = ({ searchParams } : { searchParams: {[key: string]:string | string[
                 <PageHeader title={postIndex["frontmatter"].title}/>
                 <div className="lg:flex">
                     <div className="lg:w-[70%] space-y-10">
-                    <Searchbar searchList={posts} start={start} end={end}/>
-                    <Pagination
-                        hasNextPage={end < posts.length}
-                        hasPrevPage={start > 0}
-                        totalPages={totalPages}
-                        />
+                    <Searchbar searchList={posts}/>
                     </div>
                     <div className="p-3 lg:w-[30%]">
                     <PostSidebar
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { Post } from '@/types';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import Card from './Card';
 import Link from 'next/link';
+import Pagination from './Pagination';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 
 export type SearchItem = {
@@ -15,14 +18,23 @@ export type SearchItem = {
 
 interface SearchbarProps {
   searchList: SearchItem[];
-  start: number;
-  end: number;
 }
 
-const Searchbar:React.FC<SearchbarProps> = ({ searchList, start, end }) => {
+const Searchbar:React.FC<SearchbarProps> = ({ searchList }) => {
+  
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const page = searchParams.get('page') ?? '1'
+  const per_page = searchParams.get('per_page') ?? '4'
+
+  const start = (Number(page) - 1) * Number(per_page) // 0, 5, 10 ...
+  const end = start + Number(per_page) // 5, 10, 15 ...
+
+  const totalPages = Math.ceil(searchList.length / Number(per_page));
 
   const [inputVal, setInputVal] = useState("");
-  const [searchResults, setSearchResults] = useState<SearchItem[]>([]);
+  const [searchResults, setSearchResults] = useState<SearchItem[]>(searchList);
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setInputVal(e.currentTarget.value);
@@ -31,10 +43,10 @@ const Searchbar:React.FC<SearchbarProps> = ({ searchList, start, end }) => {
   useEffect(() => {
    const filterList = searchList.filter((item) => {
       return(item.frontmatter.title.trim().toLowerCase().includes(inputVal.toLowerCase()))
-    }).slice(start,end);
+    });
     setSearchResults(filterList)
   },[inputVal,searchList])
-    
+
   return (
     <section className="w-full">
         <div className="flex my-5 gap-2 relative mx-20">
@@ -44,7 +56,7 @@ const Searchbar:React.FC<SearchbarProps> = ({ searchList, start, end }) => {
         <div className="lg:flex">
             <div className="lg:w-full space-y-5">
                 <div className="grid grid-flow-row grid-cols-2 gap-5 mx-5">    
-                    {searchResults && searchResults.map((post) => (
+                    {searchResults && searchResults.slice(start,end).map((post) => (
                         <Link key={post.fileName} href={`/blog/${post.fileName}`}>
                             <Card frontmatter={post.frontmatter} content={post.content}/>
                         </Link>
@@ -52,6 +64,11 @@ const Searchbar:React.FC<SearchbarProps> = ({ searchList, start, end }) => {
                 </div>
             </div>
         </div>
+        <Pagination
+            hasNextPage={end < searchResults.length}
+            hasPrevPage={start > 0}
+            totalPages={totalPages}
+            />
     </section>
   )
 }
