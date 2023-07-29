564886997b8a8d617ecb731b442ae629669f2b2c
KevinLim62 <cheryiong62@gmail.com>
2023-07-29 14:57:15 +0800

    changed of file path

--- src/app/blog/[posts]/page.tsx
+++ src/app/blog/[posts]/page.tsx
@@ -4,12 +4,14 @@ import { getPostData, getSinglePost, parseFrontmatter } from "@/lib/utils/mdPars
 import path from "path";
 import fs from 'fs';
 import matter from "gray-matter";
+import { GetServerSideProps } from 'next';
+import { ParsedUrlQuery } from "querystring";
 
 
-const page = async ({ params } : { params: {posts:string}}) => {
 
-  const postsTest = await getPostData(`blog/${params.posts}`);
-  
+
+const page = async ({ postData }:any) => {
+    
   const posts = {
     frontmatter: {
         title: "test",
@@ -21,16 +23,35 @@ const page = async ({ params } : { params: {posts:string}}) => {
         tags: ["string1","string2","string3"],
     },
     content:"string"
-  } 
+} 
 
-  return (
+return (
     posts && (
-      <div className='w-full px-[10%] py-10'>
-          <PageHeader title={params.posts}/>
+        <div className='w-full px-[10%] py-10'>
+          <PageHeader title={postData.fileName}/>
           <Card frontmatter={posts.frontmatter} content={`ddddd`} isSinglePost/>
       </div>
     )
-  )
+    )
 }
 
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    let postData = {};
+
+    if (params && 'posts' in params) {
+      const { posts } = params as ParsedUrlQuery;
+      // Fetch post data using getPostData function
+      postData = getPostData(`blog/${params.posts}`);
+  
+      // You should add proper error handling here if the post data is not found.
+    }
+    return {
+      props: {
+        postData,
+      },
+    };
+};
+
+
 export default page
+
