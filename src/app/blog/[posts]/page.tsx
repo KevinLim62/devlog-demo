e5884eaad70255be0e3deadd07a4030e0aeb9f8b
KevinLim62 <cheryiong62@gmail.com>
2023-07-29 15:15:19 +0800

    changed of file path

--- src/app/blog/[posts]/page.tsx
+++ src/app/blog/[posts]/page.tsx
@@ -1,42 +1,17 @@
 import Card from "@/components/Card";
 import PageHeader from "@/components/PageHeader";
-import { getPostData, getSinglePost, parseFrontmatter } from "@/lib/utils/mdParser";
-import path from "path";
-import fs from 'fs';
-import matter from "gray-matter";
-import { GetServerSideProps } from 'next';
-import { ParsedUrlQuery } from "querystring";
-
-
-interface PostProps {
-    postData?: {
-        frontmatter: any;
-        content: string;
-    };
-  }
+import { getPostData } from "@/lib/utils/mdParser";
 
 
 const page = async ({ params }: {params: {posts:string}} ) => {
    
-  const postTest = await getPostData(`blog/${params.posts}.md`);    
+  const posts = await getPostData(`blog/${params.posts}.md`);    
 
-  const posts = {
-    frontmatter: {
-        title: "test",
-        meta_title: "string",
-        description: "string",
-        image: "/string",
-        categories: ["string1","string2","string3"],
-        author: "string",
-        tags: ["string1","string2","string3"],
-    },
-    content:"string"
-} 
 
 return (
         <div className='w-full px-[10%] py-10'>
           <PageHeader title={params.posts}/>
-          {/* <Card frontmatter={posts.frontmatter} content={`ddddd`} isSinglePost/> */}
+          <Card frontmatter={posts.frontmatter} content={`ddddd`} isSinglePost/>
       </div>
     )
 }
