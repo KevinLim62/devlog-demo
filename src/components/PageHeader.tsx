1a39b59d0ce274d99b72de4130ab99390c2fe012
KevinLim62 <cheryiong62@gmail.com>
2023-08-20 23:09:40 +0800

    Linked to donjon web app

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -9,9 +9,14 @@ const PageHeader:React.FC<PageHeaderProps> = ({
 }) => {
   return (
     <section>
-        <div className="flex bg-slate-500 p-8 py-8 mb-5 text-xl h-full relative">
-          <Link className='absolute hover:scale-[1.05] transition-transform delay-100' href="/">Home</Link>
-          <h1 className="w-full text-center">
+        <div className="flex bg-slate-500 p-8 py-8 mb-5 h-full relative">
+          <div className='absolute top-6'>
+            <div className='flex gap-4'>
+              <Link className='px-4 py-2 rounded-lg bg-slate-700 text-lg hover:scale-[1.05] transition-transform delay-100' href="https://kevinlim62.github.io/donjonDraft/">Donjon</Link>
+              <Link className='px-5 py-2 rounded-lg bg-slate-700 text-lg hover:scale-[1.05] transition-transform delay-100' href="/">Home</Link>
+            </div>
+          </div>
+          <h1 className="w-full text-center text-xl">
             {title}
           </h1>
         </div>
